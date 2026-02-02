af6c32a6e7a2316309e778e8f6a9721664578bd4
basma <flowerbook100@gmail.com>
2026-02-02 00:43:06 -0800

    test basma

--- app/Http/Controllers/Admin/ShiftController.php
+++ app/Http/Controllers/Admin/ShiftController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Admin;
 use App\Http\Controllers\Controller;
 use App\Models\Shift;
 use Illuminate\Http\Request;
-
+//basma basma
 class ShiftController extends Controller
 {
     public function openShift(Request $request)
